c9fa40a942c7e9d065c9ce5fd73e4ebeb9a1946f
MatiVolpe <mati.clc.volpe@gmail.com>
2024-04-17 21:29:06 -0300

    Cambio en App para chequear funcionamiento de ramas

--- Practices-proyect/src/App.jsx
+++ Practices-proyect/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const netIncomes = [{brand: 'McDonalds', income: 1291283}, {brand: 'Burger King', income: 1927361}, {brand: 'KFC', income: 1098463}];
+
   return (
     <div>
       
