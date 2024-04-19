d222a6522765873a7dbfe72d7155d5822f04d1a2
MatiVolpe <mati.clc.volpe@gmail.com>
2024-04-18 21:08:27 -0300

    Enunciado

--- Practices-proyect/src/App.jsx
+++ Practices-proyect/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+
+// Resolver el siguiente ejercicio:
+// En el componente App.js poseo el siguiente arreglo de ingresos brutos por compañia:
+// const netIncomes = [{brand: ´McDonalds´, income: 1291283}, {brand: Burger King, income: 1927361}, {brand: ‘KFC’, income: 1098463}];
+// En el componente App:
+// Escribir un componente Table.js que arme una tabla de HTML donde las columnas sean el nombre de la marca y el ingreso neto.
+// Escribir una etiqueta p que muestre el promedio de ingreso neto entre todas las marcas. Utilizar obligatoriamente la función reduce para calcular el promedio.
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -8,7 +16,7 @@ function App() {
 
   return (
     <div>
-      
+
     </div>
   )
 }
